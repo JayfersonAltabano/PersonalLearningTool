181cb0226070ccbe3b04dce095e8853946d76a05
jayfersonaltabano02 <jayfersonaltabano02>
2022-09-25 20:57:19 +0800

    first commit

---
+++ personal-management-tool/src/test/java/com/personal/learning/personal/management/tool/PersonalManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.learning.personal.management.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
